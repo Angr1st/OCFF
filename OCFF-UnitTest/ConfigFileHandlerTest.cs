b524d3544fc7195cb013314964409f8745d02c6d
Torben Clasen <Torben.Clasen@itvt.de>
2018-03-21 10:11:37 +0100

    First UnitTetst for the ConfigFileHandler Classes InitConfigFile-Method

---
+++ OCFF-UnitTest/ConfigFileHandlerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OCFF;
+
+namespace OCFF_UnitTest
+{
+    [TestClass]
+    public class ConfigFileHandlerTest
+    {
+        [TestMethod]
+        public void InitConfigFileTestTrue()
+        {
+            string content = InitConfigFile(true);
+            Assert.AreEqual(string.Empty, content);
+        }
+
+        [TestMethod]
+        public void InitConfigFileTestFalse()
+        {
+            string content = InitConfigFile(false);
+            Assert.AreEqual("[Testing]\nis meh.", content);
+        }
+
+        private static string InitConfigFile(bool overwrite)
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> { { @"c:\Test\ConfigFile.ocff", new MockFileData("[Testing]\nis meh.") } }, "c:\\Test");
+            var sut = new ConfigFileHandler(new EmptyComputeFuncs(), new EmptyEnumerationFuncs(), fileSystem);
+            sut.InitConfigFile(overwrite);
+            var configFile = fileSystem.GetFile(@"c:\Test\ConfigFile.ocff");
+            var content = Encoding.UTF8.GetString(configFile.Contents);
+            return content;
+        }
+    }
+}
---
+++ OCFF-UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OCFF-UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OCFF-UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3fe17edc-22b1-45d6-8886-0dfc8aa68140")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
