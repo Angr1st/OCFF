7e111987edd5adfb6bf23a205071e1b32be7cd63
Torben Clasen <Torben.Clasen@itvt.de>
2018-03-22 12:07:23 +0100

    Tested the case that no ocff file exists.

--- OCFF-UnitTest/ConfigFileHandlerTest.cs
+++ OCFF-UnitTest/ConfigFileHandlerTest.cs
@@ -14,20 +14,35 @@ namespace OCFF_UnitTest
         [TestMethod]
         public void InitConfigFileTestTrue()
         {
-            string content = InitConfigFile(true);
+            string content = InitConfigFile(true, false);
+            Assert.AreEqual(string.Empty, content);
+        }
+
+        [TestMethod]
+        public void InitConfigFileTestTrueAndEmpty()
+        {
+            string content = InitConfigFile(true, true);
             Assert.AreEqual(string.Empty, content);
         }
 
         [TestMethod]
         public void InitConfigFileTestFalse()
         {
-            string content = InitConfigFile(false);
+            string content = InitConfigFile(false, false);
             Assert.AreEqual("[Testing]\nis meh.", content);
         }
 
-        private static string InitConfigFile(bool overwrite)
+        private static string InitConfigFile(bool overwrite, bool withEmpty)
         {
-            MockFileSystem fileSystem = CreateMockFileSystem();
+            MockFileSystem fileSystem;
+            if (withEmpty)
+            {
+                fileSystem = CreateEmptyMockFileSystem();
+            }
+            else
+            {
+                fileSystem = CreateMockFileSystem();
+            }  
             var sut = CreateConfigFileHandler(fileSystem);
             sut.InitConfigFile(overwrite);
             var configFile = fileSystem.GetFile(@"c:\Test\ConfigFile.ocff");
@@ -38,6 +53,12 @@ namespace OCFF_UnitTest
         private static MockFileSystem CreateMockFileSystem()
         {
             var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> { { @"c:\Test\ConfigFile.ocff", new MockFileData("[Testing]\nis meh.") } }, "c:\\Test");
+            return fileSystem;
+        }
+
+        private static MockFileSystem CreateEmptyMockFileSystem()
+        {
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> { }, "c:\\Test");
             return fileSystem;
         }
 
