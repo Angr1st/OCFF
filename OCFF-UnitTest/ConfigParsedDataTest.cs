0363f4ca23a810bd02e52a4e5e3b4b65b37ec2ae
Torben Clasen <Torben.Clasen@itvt.de>
2018-03-22 14:44:19 +0100

    Added Test for the ConfigParsedData-Class

--- OCFF-UnitTest/ConfigParsedDataTest.cs
+++ OCFF-UnitTest/ConfigParsedDataTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OCFF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,44 @@ using System.Threading.Tasks;
 
 namespace OCFF_UnitTest
 {
-    class ConfigParsedDataTest
+    [TestClass]
+    public class ConfigParsedDataTest
     {
+        [TestMethod]
+        public void GetDataStoreTest()
+        {
+            ConfigParsedData configParsedData = CreateConfigParsedData();
+            AddTestSection(configParsedData);
+            var result = configParsedData.GetDataStore();
+            Assert.IsTrue(result.ContainsKey("Test"));
+        }
 
+        [TestMethod]
+        public void AddTest()
+        {
+            ConfigParsedData configParsedData = CreateConfigParsedData();
+            AddTestSection(configParsedData);
+            Assert.IsTrue(configParsedData.KeyExsists("Test"));
+        }
+
+        [TestMethod]
+        public void AddDoubleTest()
+        {
+            ConfigParsedData configParsedData = CreateConfigParsedData();
+            AddTestSection(configParsedData);
+            AddTestSection(configParsedData);
+            Assert.IsTrue(configParsedData.KeyExsists("Test"));
+        }
+
+        private static ConfigParsedData AddTestSection(ConfigParsedData configParsedData)
+        {
+            configParsedData.Add(new ConfigSection("Test", "Value", true, new EmptyComputeFuncs(), new EmptyEnumerationFuncs()));
+            return configParsedData;
+        }
+
+        private static ConfigParsedData CreateConfigParsedData()
+        {
+            return new ConfigParsedData();
+        }
     }
 }
